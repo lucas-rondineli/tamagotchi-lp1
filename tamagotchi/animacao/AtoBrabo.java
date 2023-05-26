4dc2850595087e31fa7cc3f12b43e2eba3e12e01
gustavopaixa1 <84631468+gustavopaixa1@users.noreply.github.com>
2023-05-26 13:55:14 -0300

    Reunindo cada animação em uma classe

--- tamagotchi/animacao/AtoBrabo.java
+++ tamagotchi/animacao/AtoBrabo.java
@@ -1,6 +1,6 @@
 
-        public class AtoBrabo1 {
-            public static void esquerdo(){
+        public class AtoBrabo {
+            public static void brabo1(){
                 System.out.println("\n\n\n\n\n\n\n\n");
                 System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
                 System.out.println("x     _                                    _       _     _        x\n" +
@@ -31,5 +31,38 @@
                 System.out.println("x-----------------------------------------------------------------x");
                 System.out.println("x  1) Alimentar    2) Brincar   3)Banheiro     4)Dormir  0)Sair   x");
                 System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+    }
+
+    public static void brabo2(){
+        System.out.println("\n\n\n\n\n\n\n\n");
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        System.out.println("x     _                                    _       _     _        x\n" +
+                "x    | |                                  | |     | |   (_)       x\n" +
+                "x    | |_ __ _ _ __ ___   __ _  __ _  ___ | |_ ___| |__  _        x\n" +
+                "x    | __/ _` | '_ ` _ \\ / _` |/ _` |/ _ \\| __/ __| '_ \\| |       x\n" +
+                "x    | || (_| | | | | | | (_| | (_| | (_) | || (__| | | | |       x\n" +
+                "x     \\__\\__,_|_| |_| |_|\\__,_|\\__, |\\___/ \\__\\___|_| |_|_|       x\n" +
+                "x                               __/ |                             x\n" +
+                "x                              |___/                              x");
+        System.out.println("x.................................................................x\n" +
+                "x.........................███...........███.......................x\n" +
+                "x........................█████.........█████......................x\n" +
+                "x........................███████████████████......................x\n" +
+                "x........................███.............███......................x\n" +
+                "x........................██.████.....████.██......................x\n" +
+                "x......................██...................██....................x\n" +
+                "x......................██.........██........██....................x\n" +
+                "x........................██...█..█........██......................x\n" +
+                "x........................██...█..█........██......................x\n" +
+                "x........................██....██.........██......................x\n" +
+                "x........................██...............██......................x\n" +
+                "x..........................██..███████..██........................x\n"+
+                "x..........................██..██...██..██........................x\n" +
+                "x............................██.......██..........................x");
+        System.out.println("x.................................................................x");
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        System.out.println("x-----------------------------------------------------------------x");
+        System.out.println("x  1) Alimentar    2) Brincar   3)Banheiro     4)Dormir  0)Sair   x");
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
     }
 }
